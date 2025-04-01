88ce1dc3b295dd7fedc977790af3a46473b0e611
EduardaMarinsBrandao <duda13brandao@gmail.com>
2025-04-01 17:49:44 -0300

    código para o usuário inserir um número

---
+++ Ex06/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 06</title>
+</head>
+<body>
+<fieldset>
+    <legend>Exercício 06</legend>
+    <form method="post" action="calcular.php">
+        Digite um número:
+        <input type="text" name="n1" size="25" />
+        <br />
+
+        <button type="submit">Enviar</button>
+        <br />
+    </form>
+</fieldset>
+</body>
+</html>
