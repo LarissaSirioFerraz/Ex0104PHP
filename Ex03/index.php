19ca2b635a48f6d8b5e6ff0e4f8955a3d503997e
LarissaSirioFerraz <larissasirioferraz@gmail.com>
2025-04-01 20:00:48 -0300

    Imprimindo na tela do index.php os números de 0 a 10 com intervalos de 3 em 3.

---
+++ Ex03/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Números</title>
+</head>
+<body>
+<fieldset>
+    <legend>Números de 0 a 10 de 3 em 3</legend>
+    <br />
+<?php
+		
+		$i = 0;
+		for ($i = 0; $i <= 10; $i+=3)
+		{
+		echo " $i <br>";
+		}	
+?>
+    </form>
+</fieldset>
+</body>
+</head>
+</html>
