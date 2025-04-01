78b488b3f7f62f5709ed8d4f36a5d28fb5130eb6
EduardaMarinsBrandao <duda13brandao@gmail.com>
2025-04-01 18:24:09 -0300

    criação do index.php

---
+++ Ex10/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Exercício 10</title>
+</head>
+<body>
+<fieldset>
+    <legend>Exercício 10</legend>
+    <form method="post" action="calcular.php">
+        Digite um número:
+        <input type="text" name="n1" size="25" />
+        <br />
+        Digite o intervalo desejado:
+        <input type="text" name="n2" size="25" />
+        <br />
+
+        <button type="submit">Enviar</button>
+        <br />
+    </form>
+</fieldset>
+</body>
+</html>
