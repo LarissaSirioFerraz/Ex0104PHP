65cd4021a6b40908085b9fd280dcdf63c13566ee
EduardaMarinsBrandao <duda13brandao@gmail.com>
2025-04-01 18:14:32 -0300

    código da tabuada

---
+++ Ex08/calcular.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Resultados Exercício 07</title>
+</head>
+<body>
+<?php
+    //Declara as variáveis e captura os dados do formulário
+    $n1 = $_POST['n1'];
+
+    if ($n1 == "") {
+        echo "Você não digitou nenhum número!";
+    } 
+    else 
+    {
+        echo "A tabuada do $n1 é: <br />";
+        // Laço de repetição para a tabuada
+        for ($i = 0; $i > 10; $i++) 
+        {
+            $resultado = $n1 * $i;
+            echo "$n1 x $i = $resultado<br />";
+        }
+    } 
+?>
+</body>
+</html>
