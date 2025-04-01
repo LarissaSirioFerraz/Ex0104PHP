6101d3a4596b50d192bd024f89a489d18b738c80
LarissaSirioFerraz <larissasirioferraz@gmail.com>
2025-04-01 20:18:28 -0300

    Imprimindo na tela do index.php pedindo a inserção de um dado para repeti-lo.

---
+++ Ex04/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Repetição</title>
+</head>
+<body>
+<fieldset>
+    <legend>Repetição</legend>
+    <form method="post" action="calcular.php">
+        Digite algo para repetir 10 vezes:
+        <input type="text" name="nome" size="25" />
+		<br />
+		<button type="submit">Repetir 10x</button>
+		<br />
+		
+    </form>
+</fieldset>
+</body>
+</head>
+</html>
