09ffe480b58d0ec092c06ccc8e1d1e188cea92af
LarissaSirioFerraz <larissasirioferraz@gmail.com>
2025-04-01 20:28:34 -0300

    Imprimindo na tela do index.php pedindo a inserção de um dado e quantas vezes o mesmo irá se repetir.

---
+++ Ex05/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Repetição</title>
+</head>
+<body>
+<fieldset>
+    <legend>Repetição</legend>
+    <form method="post" action="calcular.php">
+        Digite algo:
+        <input type="text" name="algo" size="25" />
+		<br />
+		 Quantas vezes você quer repeti-la?
+        <input type="text" name="vezes" size="25" />
+		<br />
+		<button type="submit">Repetir</button>
+		<br />
+		
+    </form>
+</fieldset>
+</body>
+</head>
+</html>
