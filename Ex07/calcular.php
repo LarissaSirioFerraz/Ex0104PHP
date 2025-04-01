035ca2652537e50f1b73ad57d9fbcf5c9bbae41d
EduardaMarinsBrandao <duda13brandao@gmail.com>
2025-04-01 18:06:10 -0300

    criação do código do for

---
+++ Ex07/calcular.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Resultados Exercício 07</title>
+</head>
+<body>
+<?php
+    //Declara as variáveis e captura os dados do formulário
+    $n1 = $_POST['n1'];
+
+    if ($n1 == "") {
+        echo "Você não digitou nenhum número!";
+    } 
+    else 
+    {
+        echo "Os números pares entre $n1 e 0 são: <br />";
+        // Laço de repetição para verificar os números pares
+        for ($i = $n1; $i = 0; $i--) 
+        {
+            if ($i % 2 == 0) 
+            {
+                echo "$i<br />";
+            } 
+        }
+    } 
+?>
+</body>
+</html>
